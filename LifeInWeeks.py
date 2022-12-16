76bbf357ec38b9c8513fd86fb8f7442116067a91
Shmielen <102589675+Shmielen@users.noreply.github.com>
2022-12-16 22:09:28 +0200

    Day 2 exercise demonstrating the use of fstrings

---
+++ LifeInWeeks.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+yearsleft = 90-int(age)
+monthsleft = yearsleft*12
+weeksleft = yearsleft*52
+daysleft = yearsleft*365
+
+print(f"You have {daysleft} days, {weeksleft} weeks, and {monthsleft} months left.")
+
+
