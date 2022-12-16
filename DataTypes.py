dbec749699672741aa72fc7450706eed8c51d8a9
Shmielen <102589675+Shmielen@users.noreply.github.com>
2022-12-16 21:34:54 +0200

    Day 2 Data Types exercise
    This code shows how data types can be converted to preform different functions

---
+++ DataTypes.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+int_num1 = int(two_digit_number[0])
+int_num2 = int(two_digit_number[1])
+
+newnum = int_num1 + int_num2
+print(newnum)
